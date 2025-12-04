0227a6fcc8f644f7374fbac6c280f0081b87463a
jayceeasparagus <118419528+jayceeasparagus@users.noreply.github.com>
2025-12-03 19:15:10 -0800

    Modified both files for readability and documentation

--- algorithm.py
+++ algorithm.py
@@ -2,23 +2,29 @@ import numpy as np
 from scipy.io import wavfile
 
 def iir(x, b, a):
-    print("iir")
+    """Function IIR takes in coefficient lists b and a along with input signal x
+    and returns output y
+    
+    output reflects the iir filter, or infinite impulse response filter"""
+
+    # define
     x_len = len(x)
     y = np.zeros(x_len)
-    
-    # normalize
+
+    # normalize coefficients
     b = np.array(b) / a[0]
     a = np.array(a) / a[0]
 
+    # replicate summation
     for n in range(x_len):
+        # look at inputs
         for k in range(len(b)):
             if n - k >= 0:
                 y[n] += b[k] * x[n-k]
-                # print(y[n])
 
+        # look at outputs
         for k in range(1, (len(a))):
             if n - k >= 0:
                 y[n] -= a[k] * y[n-k]
-                # print(y[n])
 
     return y
--- main.py
+++ main.py
@@ -5,61 +5,61 @@ import matplotlib.pyplot as plt
 from numpy.fft import fft, fftfreq
 import algorithm
 
+# change variable for cutoff
 CUTOFF = 500
 
+# get audio
 fs, audio = wavfile.read("test_audio.wav")
 
-# describe audio file
-print("Sample rate:", fs)
-print("Shape:", audio.shape)
-
-# make it monotone
+# monotone
 if audio.ndim == 2:
     audio = audio.mean(axis=1)
 
 # normalize audio
 audio = audio / np.max(np.abs(audio))
 
+# get Butterworth coefficients
 b, a = butter(4, CUTOFF, btype="low", fs=fs)
 
+# create modified audio
 audio2 = algorithm.iir(audio, b, a)
 
-# plot waveform
+# plot original waveform
 t = np.arange(len(audio)) / fs
 plt.figure(figsize=(12, 4))
 plt.plot(t, audio)
-plt.title("Waveform (Time Domain)")
+plt.title("Original Waveform")
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.show()
 
-# plot waveform
+# plot modified waveform
 t = np.arange(len(audio2)) / fs
 plt.figure(figsize=(12, 4))
 plt.plot(t, audio2)
-plt.title("Waveform (Time Domain)")
+plt.title("Modified Waveform")
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
 plt.show()
 
-# plot frequency graph
+# plot original frequency graph
 N = len(audio)
 Y = fft(audio)
 freqs = fftfreq(N, 1/fs)
 plt.figure(figsize=(12,4))
-plt.plot(freqs[:N//2], np.abs(Y[:N//2]))  # only positive frequencies
-plt.title("Frequency Spectrum (Magnitude)")
+plt.plot(freqs[:N//2], np.abs(Y[:N//2]))
+plt.title("Original Frequency Graph")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude")
 plt.show()
 
-# plot frequency graph
+# plot modified frequency graph
 N = len(audio2)
 Y = fft(audio2)
 freqs = fftfreq(N, 1/fs)
 plt.figure(figsize=(12,4))
-plt.plot(freqs[:N//2], np.abs(Y[:N//2]))  # only positive frequencies
-plt.title("Frequency Spectrum (Magnitude)")
+plt.plot(freqs[:N//2], np.abs(Y[:N//2]))
+plt.title("Modified Frequency Graph")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude")
 plt.show()
